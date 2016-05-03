703ba3f1fc30655e7d0bf97643898fcce70599af
jordane-quincy <jordanequincy@gmail.com>
2016-05-03 09:28:07 +0200

    premier commit android

---
+++ android/app/src/main/java/com/github/jordane_quincy/bataillecartes/LoginActivity.java
@@ -0,0 +1,84 @@
+package com.github.jordane_quincy.bataillecartes;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * A login screen that offers login
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    /**
+     * Id to identity READ_CONTACTS permission request.
+     */
+    private static final int REQUEST_READ_CONTACTS = 0;
+
+    // UI references.
+    private AutoCompleteTextView mLoginView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mLoginView = (AutoCompleteTextView) findViewById(R.id.email);
+        // populateAutoComplete();
+
+        Button mSaveButton = (Button) findViewById(R.id.save_button);
+        mSaveButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveInfos();
+            }
+        });
+
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_READ_CONTACTS) {
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //populateAutoComplete();
+            }
+        }
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private void saveInfos() {
+        // Reset errors.
+        mLoginView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String login = mLoginView.getText().toString();
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(login)) {
+            mLoginView.setError(getString(R.string.error_field_required));
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+
+
+            Toast.makeText(getApplicationContext(),"login : "+login, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
+
